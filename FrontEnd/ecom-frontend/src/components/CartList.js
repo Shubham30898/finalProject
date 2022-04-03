78c39dd762485950e9e2c0d6ff2b0c80faeeff3f
Shubham <shubham30898@gmail.com>
2022-04-03 17:59:56 +0530

    cartlist added

--- FrontEnd/ecom-frontend/src/components/CartList.js
+++ FrontEnd/ecom-frontend/src/components/CartList.js
@@ -1,37 +1,65 @@
 import React from 'react'
 import axios from 'axios'
 import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function CartList() {
   const [data, setData] = useState([])
+  const [totalamount, setTotalAmount] = useState([])
   useEffect(() => {
     axios
       .get(
         'http://localhost:8080/cart/?token=814f6289-d38a-4867-8cac-f1700df85457',
       )
       .then((res) => {
-        console.log(res.data)
-
-        setData(res.data)
+        setData(res.data.cartItems)
+        setTotalAmount(res.data)
       })
       .catch((error) => {
         console.log(error)
         alert('something went wrong')
       })
   }, [])
+
+  console.log(data)
+  console.log(totalamount.totalCost)
+  const navigate = useNavigate()
   return (
     <div>
       CartList:
-      {data.map((info) => (
-        <div>
-          <p>{info.quantity}</p>
-          <img src={info.product.imageUrl} alt="category"></img>
-          <h1>{info.product.name}</h1>
-          <p>{info.product.description}</p>
-          <p>{info.product.price}</p>
-          <p>{info.totalCost}</p>
-        </div>
-      ))}
+      {/* {data.cartItems.map((info) => {
+        return (
+          <div>
+            <p>{info.id}</p>
+
+            <p> Qunatity is: {info.quantity}</p>
+            {info.product.map((sub) => {
+              return (
+                <div>
+                  <p>sub.name</p>
+                  <img src={sub.imageURL} alt="product"></img>
+                  <p>{sub.price}</p>
+                </div>
+              )
+            })}
+          </div>
+        )
+      })}
+      <p>Total Amount: {data.totalCost}</p> */}
+      <div>
+        {data.map((info) => (
+          <div>
+            <p>{info.product.name}</p>
+            <img src={info.product.imageURL} alt="product"></img>
+            <p>Description:{info.product.description}</p>
+            <p>Qunatity:{info.quantity}</p>
+            <p>Price:{info.product.price}</p>
+          </div>
+        ))}
+      </div>
+      <p>Total Amount :{totalamount.totalCost}</p>
+      <br />
+      <button onClick={() => navigate('/address')}>Add Address</button>
     </div>
   )
 }
--- FrontEnd/ecom-frontend/src/components/Navbar.js
+++ FrontEnd/ecom-frontend/src/components/Navbar.js
@@ -1,13 +1,23 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
 
 function Navbar() {
+  const navigate = useNavigate()
+
+  const handleLogout = () => {
+    sessionStorage.removeItem('token')
+    navigate('/', { replace: true })
+  }
+
   return (
     <nav>
       <NavLink to="/">Home</NavLink>
       <NavLink to="/login">Login</NavLink>
       <NavLink to="/signup">SignUp</NavLink>
       <NavLink to="/cart">Cart</NavLink>
+      {sessionStorage.getItem('token') ? (
+        <button onClick={handleLogout}>Sign Out</button>
+      ) : null}
     </nav>
   )
 }
